4b32d12856b6a09a102f5bb37812fa583a558100
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-04 19:27:56 +0300

    add service :smile:

---
+++ src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { URLSearchParams, Jsonp } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AppService {
+  constructor(private jsonp: Jsonp) {}
+
+  search(term: string) {
+    const search = new URLSearchParams();
+    search.set('action', 'opensearch');
+    search.set('search', term);
+    search.set('format', 'json');
+
+    return this.jsonp
+      .get('https://en.wikipedia.org/w/api.php?callback=JSONP_CALLBACK', {
+        search,
+      })
+      .map(response => response.json()[1]);
+  }
+}
