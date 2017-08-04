07a0c43aed9adeb2e008fc2e9d7935e6319ef1b1
ragmha <Abul.MidhatHasanRaghib@metropolia.fi>
2017-08-04 19:32:26 +0300

    add component :smile:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AppService } from './app.service';
+
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  items: Array<string>;
+
+  constructor(private service: AppService) {}
+
+  search(term: string) {
+    this.service.search(term).subscribe(results => (this.items = results));
+  }
 }
